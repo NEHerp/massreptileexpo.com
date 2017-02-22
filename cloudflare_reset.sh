379edc8974e7e223b75906d53d026aef2559a673
Dan Rowe <drowe@wayfair.com>
2017-02-21 20:23:04 -0500

    removed debug line

--- cloudflare_reset.sh
+++ cloudflare_reset.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-echo $Testing
 curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/purge_cache" \
      -H "X-Auth-Email: webmaster@neherp.com" -H "X-Auth-Key: $API_KEY" -H "Content-Type: application/json" --data '{"purge_everything":true}'
