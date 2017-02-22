9670595031351240acbbefc09257e654193971df
Dan Rowe <drowe@wayfair.com>
2017-02-21 20:01:44 -0500

    add test variable

--- cloudflare_reset.sh
+++ cloudflare_reset.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+echo $Testing
 curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/purge_cache" \
      -H "X-Auth-Email: webmaster@neherp.com" -H "X-Auth-Key: $API_KEY" -H "Content-Type: application/json" --data '{"purge_everything":true}'
